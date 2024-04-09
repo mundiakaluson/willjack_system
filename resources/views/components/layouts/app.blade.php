c405bcaba5223f529f09f5c1e957273e0819d987
Jack <mundiakaluson@gmail.com>
2024-04-10 00:52:06 +0100

    some stuff

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,9 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
+        @vite('resources/css/app.css')
     </head>
     <body>
-        Header
+        @livewire('navbar')
         {{ $slot }}
         Footer
     </body>
