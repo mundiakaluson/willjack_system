d2bdb71a997b96d4ce97557f3f6d0d373d40c40f
Jack <mundiakaluson@gmail.com>
2024-04-21 17:28:34 +0100

    color change:

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -22,7 +22,7 @@
         <hr class="my-6 border-gray-200 md:my-10 dark:border-gray-700" />
 
         <div class="flex flex-col items-center sm:flex-row sm:justify-between">
-            <p class="text-sm text-gray-500 dark:text-gray-300">© Copyright 2021. All Rights Reserved.</p>
+            <p class="text-sm text-gray-500 dark:text-gray-300">© Copyright <?php echo date("Y M"); ?>. All Rights Reserved.</p>
 
             <div class="flex -mx-2">
                 <a href="#" class="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400" aria-label="Reddit">
--- resources/views/livewire/hero.blade.php
+++ resources/views/livewire/hero.blade.php
@@ -3,11 +3,11 @@
             <div class="items-center lg:flex">
                 <div class="w-full lg:w-1/2">
                     <div class="lg:max-w-lg">
-                        <h1 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Let us help you to choose <br> your <span class="text-gray-400 hover:text-sky-500">New Home</span></h1>
+                        <h1 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Let us help you to choose <br> your <span class="text-gray-400 hover:text-slate-500">New Home</span></h1>
 
                         <p class="mt-3 text-gray-600 dark:text-gray-400">We are a well-connected and committed Real Estate firm capable of providing tailored options to every landlord and tenant!</p>
 
-                        <button class="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-gray-400 rounded-lg lg:w-auto hover:bg-sky-500 focus:outline-none focus:bg-sky-500">Get Started</button>
+                        <button class="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-gray-400 rounded-lg lg:w-auto hover:bg-slate-500 focus:outline-none focus:bg-sky-100">Get Started</button>
                     </div>
                 </div>
 
