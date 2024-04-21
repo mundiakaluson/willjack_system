82738a6d3ddb0c086007ddb1f1103351e20fc0ea
Jack <mundiakaluson@gmail.com>
2024-04-21 17:10:04 +0100

    color pallets

--- resources/views/livewire/hero.blade.php
+++ resources/views/livewire/hero.blade.php
@@ -1,18 +1,18 @@
 <header>
-    <div class="container px-6 py-16 mx-auto">
+    <div class="container w-2/3 py-8 mx-auto">
             <div class="items-center lg:flex">
                 <div class="w-full lg:w-1/2">
                     <div class="lg:max-w-lg">
-                        <h1 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Best place to choose <br> your <span class="text-blue-500 ">clothes</span></h1>
+                        <h1 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Best place to choose <br> your <span class="text-gray-400 ">clothes</span></h1>
 
                         <p class="mt-3 text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro beatae error laborum ab amet sunt recusandae? Reiciendis natus perspiciatis optio.</p>
 
-                        <button class="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-lg lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Shop Now</button>
+                        <button class="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-gray-400 rounded-lg lg:w-auto hover:bg-sky-500 focus:outline-none focus:bg-sky-500">Shop Now</button>
                     </div>
                 </div>
 
                 <div class="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
-                    <img class="w-full h-full lg:max-w-3xl" src="https://merakiui.com/images/components/Catalogue-pana.svg" alt="Catalogue-pana.svg">
+                    <img class="w-full h-full lg:max-w-3xl" src="{{ url('vectors/hero.svg') }}" alt="Catalogue-pana.svg">
                 </div>
             </div>
     </div>
