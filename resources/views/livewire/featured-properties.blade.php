de31b1c57df9881284b2d4e82c04a439869b7579
Jack <mundiakaluson@gmail.com>
2024-04-21 20:26:28 +0100

    gradient for featured

--- resources/views/livewire/featured-properties.blade.php
+++ resources/views/livewire/featured-properties.blade.php
@@ -1,7 +1,7 @@
 <div>
-    <div class="bg-white">
+    <div class="hover:bg-gradient-to-b from-white from-30% via-slate-100 to-gray-100 transition ease-in-out delay-200  border-slate-500 mt-10">
       <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 class="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900">Featured Properties</h2>
 
         <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           <div class="group relative">
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,5 +1,5 @@
 <div>
     <livewire:hero />
-    <livewire:hero />
+    <livewire:features />
     <livewire:featured-properties />
 </div>
