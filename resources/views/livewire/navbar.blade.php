7817420ef20a893c8f05195ce372bfef358bb3a2
Jack <mundiakaluson@gmail.com>
2024-04-24 21:04:03 +0100

    fixed the damn logo bug, fuckin hell

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -1,12 +1,14 @@
 <nav x-data="{ isOpen: false }" class="relative bg-white dark:bg-gray-800">
     <div class="container px-6 py-8 mx-auto md:flex">
-        <div class="items-center justify-between flex flex-col sm:items-center">
-            <a href="#" class="w-full h-full">
-                <img class="w-auto h-7" src="{{ url('images/WJ.png') }}" alt="">
-            </a>
+        <div class="items-center justify-between flex flex-col">
+            <div class="flex sm:justify-center pb-4">
+                <a href="#" class="w-full h-full">
+                    <img class="w-auto h-10" src="{{ url('images/WJ.png') }}" alt="">
+                </a>
+            </div>
 
             <!-- Mobile menu button -->
-            <div class="flex lg:hidden">
+            <div class="flex lg:hidden sm:items-right">
                 <button x-cloak @click="isOpen = !isOpen" type="button" class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
                     <svg x-show="!isOpen" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
